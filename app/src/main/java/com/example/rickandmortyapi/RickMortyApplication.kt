b541a28ec76586d8a44ae381cf8c4e308b0fbb95
JGaldp-beep <juan.galdo@ucb.edu.bo>
2025-05-16 10:31:46 -0400

    Improving code

--- app/src/main/java/com/example/rickandmortyapi/RickMortyApplication.kt
+++ app/src/main/java/com/example/rickandmortyapi/RickMortyApplication.kt
@@ -1,21 +1,13 @@
 package com.example.rickandmortyapi
 
 import android.app.Application
-import androidx.work.Configuration
 import com.example.rickandmortyapi.data.AppContainer
 import com.example.rickandmortyapi.data.DefaultAppContainer
-import com.example.rickandmortyapi.workers.RickAndMortyWorkerFactory
 
-class RickMortyApplication : Application(), Configuration.Provider {
+class RickMortyApplication : Application() {
     lateinit var container: AppContainer
     override fun onCreate() {
         super.onCreate()
         container = DefaultAppContainer(this)
     }
-
-    override val workManagerConfiguration: Configuration
-        get() = Configuration.Builder()
-            .setWorkerFactory(RickAndMortyWorkerFactory(container.charactersRepository))
-            .build()
-
 }
